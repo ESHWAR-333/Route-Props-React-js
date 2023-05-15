f55d28f2a4d85e2ada85deb00d6dd0414aa9d65e
Eshwar <chalamallan3@gmail.com>
2023-05-15 19:00:09 +0000

    first commit

---
+++ src/components/BlogItemDetails/index.js
@@ -0,0 +1,78 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import './index.css'
+
+// const blogData = {
+//   title: 'Blog Name',
+//   imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-3-img.png',
+//   avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
+//   author: 'Author Name',
+//   content:
+//     'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+// }
+
+class BlogItemDetails extends Component {
+  state = {blogData: {}, isLoading: true}
+
+  componentDidMount() {
+    this.getBlogDetails()
+  }
+
+  getBlogDetails = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    const data = await response.json()
+
+    const updateData = {
+      author: data.author,
+      avatarUrl: data.avatar_url,
+      content: data.content,
+      id: data.id,
+      imageUrl: data.image_url,
+      title: data.title,
+      topic: data.topic,
+    }
+
+    this.setState({blogData: updateData, isLoading: false})
+  }
+
+  renderBlogItemDetails = () => {
+    const {blogData, isLoading} = this.state
+    const {title, imageUrl, content, avatarUrl, author} = blogData
+    return (
+      <div className="blog-info">
+        {isLoading ? (
+          <Loader
+            type="TailSpin"
+            color="#00BFFF"
+            height={50}
+            width={50}
+            text-center
+          />
+        ) : (
+          <>
+            <h2 className="blog-details-title">{title}</h2>
+            <div className="author-details">
+              <img className="author-pic" src={avatarUrl} alt={author} />
+              <p className="details-author-name">{author}</p>
+            </div>
+
+            <img className="blog-image" src={imageUrl} alt={title} />
+            <p className="blog-content">{content}</p>
+          </>
+        )}
+      </div>
+    )
+  }
+
+  render() {
+    return <div className="blog-container">{this.renderBlogItemDetails()}</div>
+  }
+}
+
+export default BlogItemDetails
